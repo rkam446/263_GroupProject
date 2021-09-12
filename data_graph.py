e0a1f8ada87577913becc6fa3a93a0ccc494b96f
AurelianoAureliano <80793406+rkam446@users.noreply.github.com>
2021-09-12 15:18:05 +1200

    figure1

---
+++ data_graph.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t_nitrate, nitrate = np.genfromtxt("data/nl_n.csv", delimiter=",", skip_header=1, unpack=True)
+t_cattle, cattle = np.genfromtxt("data/nl_cows.txt", delimiter=",", skip_header=1, unpack=True)
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel("Year")
+ax1.set_ylabel("Nitrate Concentration (mg/L)")
+ax1.scatter(t_nitrate, nitrate, label="Nitrate", c="red")
+ax1.scatter([], [], label="Cattle", c="black")
+ax2 = ax1.twinx()
+ax2.set_ylabel("Cattle Numbers")
+ax2.scatter(t_cattle, cattle, label="Cattle", c="black")
+ax1.legend()
+plt.show()
